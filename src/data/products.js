1a542c62c717e7346feb26336fa4ca7ba8b85f8e
lenzMoraga <49219425+lenz-moraga@users.noreply.github.com>
2024-02-21 18:01:06 -0600

    adding the recommended sections, WIP

---
+++ src/data/products.js
@@ -0,0 +1,30 @@
+export const PRODUCTS_INFORMATION = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: "1,000.00$",
+    imgUrl: "/Product_1.png",
+    href: "product/1",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: "2,000.00$",
+    imgUrl: "/Product_2.png",
+    href: "product/2",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: "3,000.00$",
+    imgUrl: "/Product_3.png",
+    href: "product/3",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: "4,000.00$",
+    imgUrl: "/Product_4.png",
+    href: "product/4",
+  },
+];
