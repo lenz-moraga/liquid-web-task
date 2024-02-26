6d1b8a94f0aea54604c4261996c680fd9267f426
lenzMoraga <49219425+lenz-moraga@users.noreply.github.com>
2024-02-26 08:33:42 -0600

    add the review section, WIP

--- src/data/products.js
+++ src/data/products.js
@@ -28,3 +28,6 @@ export const PRODUCTS_INFORMATION = [
     href: "product/4",
   },
 ];
+
+export const REVIEW_COMMENTS =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
